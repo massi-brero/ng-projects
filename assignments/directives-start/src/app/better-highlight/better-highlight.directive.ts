4188b17d76c1f2abf1e9302c08695e6a1a6ab6d5
massimiliano@adess.de <massimiliano@adess.de>
2020-01-08 18:44:49 +0100

    finished services in main application

--- assignments/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ assignments/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(event: Event) {
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(event: Event) {
+    this.backgroundColor = this.defaultColor;
+  }
 
 }
--- food-lists/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ food-lists/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import {RecipeService} from "../../recipe.service";
 
 @Component({
   selector: 'app-recipes-item',
@@ -10,13 +11,13 @@ export class RecipesItemComponent implements OnInit {
   @Input() recipe: Recipe;
   @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
   public onSelectRecipe() {
-    this.recipeSelected.emit();
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
