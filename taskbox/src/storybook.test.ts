4d229830d3658fc7ba533f5ae22b5d5b905789e7
massimiliano@adess.de <massimiliano@adess.de>
2020-01-10 14:21:33 +0100

    simple component works

--- taskbox/src/storybook.test.ts
+++ taskbox/src/storybook.test.ts
@@ -1,8 +1,10 @@
 import * as path from 'path';
-import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
+import initStoryshots, {
+  multiSnapshotWithOptions,
+} from '@storybook/addon-storyshots';
 
 initStoryshots({
   framework: 'angular',
-  configPath: path.join(__dirname, '../storybook/'),
+  configPath: path.join(__dirname, '../.storybook'),
   test: multiSnapshotWithOptions(),
 });
