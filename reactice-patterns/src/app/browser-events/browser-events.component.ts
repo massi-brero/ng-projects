dec2ac370313c4a617ea78674e638c48053c8cfe
massi.brero@gmail.com <massi.brero@gmail.com>
2020-02-03 22:25:22 +0100

    starting reactive patterns

---
+++ reactice-patterns/src/app/browser-events/browser-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'browser-events',
+  templateUrl: './browser-events.component.html',
+  styleUrls: ['./browser-events.component.css']
+})
+export class BrowserEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
