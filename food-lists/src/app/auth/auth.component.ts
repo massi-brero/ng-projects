4bf459df6e3be3fa9abc47fe6629cc262fd335bf
massi.brero@gmail.com <massi.brero@gmail.com>
2020-07-14 22:06:56 +0200

    building modules 1

--- food-lists/src/app/auth/auth.component.ts
+++ food-lists/src/app/auth/auth.component.ts
@@ -1,27 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    ComponentFactoryResolver,
+    ViewChild,
+    OnDestroy,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
     isLoginMode = true;
     isLoading = false;
     error: string = null;
+    @ViewChild(PlaceholderDirective, { static: false })
+    alertHost: PlaceholderDirective;
+    closeSub: Subscription;
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private componentFactoryResover: ComponentFactoryResolver
+    ) {}
+
+    ngOnDestroy() {
+        if (this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
+    }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onHandleError() {
-        console.log('close2');
         this.error = null;
+    }
+
+    private showErrorAlert(message: string) {
+        const alertComponentFactory = this.componentFactoryResover.resolveComponentFactory(
+            AlertComponent
+        );
+        const hostViewContainerRef = this.alertHost.viewRef;
+        hostViewContainerRef.clear();
+
+        const componentRef = hostViewContainerRef.createComponent(
+            alertComponentFactory
+        );
+        componentRef.instance.message = message;
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        });
     }
 
     onSubmit(form: NgForm) {
@@ -47,6 +84,7 @@ export class AuthComponent {
             },
             (error) => {
                 this.error = error;
+                this.showErrorAlert(error);
                 this.isLoading = false;
             }
         );
