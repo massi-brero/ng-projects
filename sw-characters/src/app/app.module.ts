9c9feeb3dd1ccf750bd61110988a1eec64e9d48a
Massimiliano Brero <Massimiliano Brero>
2021-06-19 16:55:07 +0200

    navigation 1form validation

--- sw-characters/src/app/app.module.ts
+++ sw-characters/src/app/app.module.ts
@@ -10,19 +10,33 @@ import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
 import { CreateCharacterComponent } from './create-character/create-character.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsComponent
+  },
+  {
+    path: 'new-character',
+    component: CreateCharacterComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent
+    CreateCharacterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
