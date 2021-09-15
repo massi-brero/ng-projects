b9e9e09421b9563be334cf8c3151e6d6999ab008
massi.brero@gmail.com <massi.brero@gmail.com>
2021-09-15 22:01:39 +0200

    beginner 1

--- AngularComponents101/src/app/countdown/countdown.component.ts
+++ AngularComponents101/src/app/countdown/countdown.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
+export class CountdownComponent implements OnInit{
+
+  @Input() init: number  = null;
+  public counter = 0;
 
   constructor() { }
 
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  private startCountdown() {
+    if (this.init && this.init > 0) {
+       this.counter = this.init;
+       this.doCountdown();
+    }
+  }
+
+  private doCountdown() {
+    setTimeout(() => {
+      this.counter = --this.counter;
+      this.processCount();
+    }, 1000);
+  }
+
+  private processCount() {
+    // emit event Count
+    console.log('count: ', this.counter);
+
+    if (this.counter === 0) {
+      // emit counter
+      console.log('-- counter end --');
+    } else {
+      this.doCountdown();
+    }
+  }
 }
--- AngularComponents101/src/app/progress-bar/progress-bar.component.ts
+++ AngularComponents101/src/app/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent {
-
+  @Input() progress = 0;
   constructor() { }
 
 }
