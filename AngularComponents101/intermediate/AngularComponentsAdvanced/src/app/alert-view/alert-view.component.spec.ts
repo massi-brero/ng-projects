87618c90e6e3cdf4d07b6e881889a0728c899bb2
massi.brero@gmail.com <massi.brero@gmail.com>
2021-09-24 21:38:09 +0200

    updat to ng 12

--- AngularComponents101/intermediate/AngularComponentsAdvanced/src/app/alert-view/alert-view.component.spec.ts
+++ AngularComponents101/intermediate/AngularComponentsAdvanced/src/app/alert-view/alert-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AlertViewComponent } from './alert-view.component';
 
@@ -6,7 +6,7 @@ describe('AlertViewComponent', () => {
   let component: AlertViewComponent;
   let fixture: ComponentFixture<AlertViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AlertViewComponent ]
     })
