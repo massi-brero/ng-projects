12460bfbaf0f06decb5c52bfcf507de157add64b
massi.brero@gmail.com <massi.brero@gmail.com>
2021-10-12 22:26:51 +0200

    forms: templates 1

---
+++ forms/angular-forms-course/src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  val = {
+    email: 'hello@gmail.com',
+    password: '123456'
+  };
+
+  constructor() {
+
+
+  }
+
+  ngOnInit() {
+  }
+
+  login(loginForm: NgForm, $e) {
+    console.log(loginForm.errors, loginForm.valid, $e);
+    console.log('val: ', this.val);
+  }
+}
