430b8a59067d7a527d15027b2c3a06fe4d7a53bf
massi.brero@gmail.com <massi.brero@gmail.com>
2021-10-30 11:48:27 +0200

    forms: file uploader 7

--- forms/angular-forms-course/src/app/address-form/address-form.component.ts
+++ forms/angular-forms-course/src/app/address-form/address-form.component.ts
@@ -1,26 +1,57 @@
-import {Component, Input} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Component, Input, OnDestroy} from '@angular/core';
+import {ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'address-form',
     templateUrl: './address-form.component.html',
     styleUrls: ['./address-form.component.scss'],
-    providers: []
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        multi: true,
+        useExisting: AddressFormComponent
+    }]
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements ControlValueAccessor, OnDestroy {
+
+    constructor(private fb: FormBuilder) {
+    }
+
     @Input() legend: string;
+    onChangeSubscription: Subscription;
     form: FormGroup = this.fb.group({
         addressLine1: [null, [Validators.required]],
         addressLine2: [null, [Validators.required]],
         zipCode: [null, [Validators.required]],
         city: [null, [Validators.required]]
     });
-
-    constructor(private fb: FormBuilder) {
+    onTouched = () => {
     }
 
-    onTouched() {
+    ngOnDestroy(): void {
+        this.onChangeSubscription.unsubscribe();
+    }
 
+    registerOnChange(onChange: any): void {
+        this.onChangeSubscription = this.form.valueChanges.subscribe(onChange);
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+        if (isDisabled) {
+            this.form.disable();
+        } else {
+            this.form.enable();
+        }
+    }
+
+    writeValue(value: any): void {
+        if (value) {
+            this.form.setValue(value);
+        }
     }
 }
 
