7b5507d6b5415c0e661eb69e9ccdc6f30dc2a83f
massi.brero@gmail.com <massi.brero@gmail.com>
2021-11-10 21:59:09 +0100

    deleted old projects and started monorepo bee

--- forms/angular-forms-course/src/app/create-course/create-course.component.ts
+++ forms/angular-forms-course/src/app/create-course/create-course.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 
 
-
-
 @Component({
-  selector: 'create-course',
-  templateUrl: './create-course.component.html',
-  styleUrls: ['./create-course.component.scss'],
-  providers: [
-      {
-          provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError:true}
-      }
-  ]
+    selector: 'create-course',
+    templateUrl: './create-course.component.html',
+    styleUrls: ['./create-course.component.scss'],
+    providers: [
+        {
+            provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
+        }
+    ]
 })
-export class CreateCourseComponent  {
+export class CreateCourseComponent {
 
+    submit(step1, step2, step3) {
+        console.log(step1, step2, step3);
+    }
 }
