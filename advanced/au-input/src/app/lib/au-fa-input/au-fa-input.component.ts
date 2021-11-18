1d1da02eba94f72ba097a20d1ecb91cab183635e
massi.brero@gmail.com <massi.brero@gmail.com>
2021-11-18 23:53:30 +0100

    advanced: component styling 2

--- advanced/au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
+++ advanced/au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, Component, ContentChild, HostBinding, Input, OnInit, ViewEncapsulation,} from '@angular/core'
+import {AfterContentInit, Component, ContentChild, HostBinding, Input, OnInit, ViewEncapsulation} from '@angular/core'
 import {InputRefDirective} from '../common/input-ref.directive'
 
 @Component({
