94756f21d8376deb028ace81228aa13471e0d784
massi.brero@gmail.com <massi.brero@gmail.com>
2021-11-21 15:05:07 +0100

    advanced: component testing 2

--- advanced/au-input/src/app/app.component.spec.ts
+++ advanced/au-input/src/app/app.component.spec.ts
@@ -1,25 +1,64 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component'
+import { InputRefDirective } from './lib/common/input-ref.directive'
+import { DebugElement } from '@angular/core'
+import { By } from '@angular/platform-browser'
 
 describe('AppComponent', () => {
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                AppComponent
-            ],
-        }).compileComponents();
-    }));
+    let component: AuFaInputComponent,
+        fixture: ComponentFixture<AppComponent>,
+        el: DebugElement,
+        auInputEmail
 
-    it('should create the app', waitForAsync(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
-    }));
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [
+                    AppComponent,
+                    AuFaInputComponent,
+                    InputRefDirective,
+                ],
+            }).compileComponents()
+        })
+    )
 
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent)
+        component = fixture.debugElement.componentInstance
+        el = fixture.debugElement
+        auInputEmail = el.query(By.css('au-fa-input[elemid="email"]'))
+    })
 
+    it(
+        'should create the app',
+        waitForAsync(() => {
+            expect(component).toBeTruthy()
+        })
+    )
 
+    describe('test for generation of au input compinent', () => {
 
+      beforeEach(() => {
+        fixture.detectChanges()
+      })
 
+      it(
+        'should create a font awesome email au input component',
+        waitForAsync(() => {
+          expect(auInputEmail).toBeTruthy()
+        })
+      )
 
-});
+      it(
+        'should create a font awesome email input element',
+        waitForAsync(() => {
+          const emailComp = auInputEmail.query(By.css('#email'))
+          expect(emailComp).toBeTruthy()
+          const icon = auInputEmail.query(By.css('i.icon.fa.fa-envelope'))
+          expect(icon).toBeTruthy()
+        })
+      )
+    })
+})
