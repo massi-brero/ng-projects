75fa7c9090eb4c65c52fdee045d42404649258d0
massi.brero@gmail.com <massi.brero@gmail.com>
2021-12-09 23:20:44 +0100

    advanced: modal 3

---
+++ advanced/au-modal/au-modal/src/app/au-modal/au-modal-open-on-click.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[auModalOpenOnClick]'
+})
+export class AuModalOpenOnClickDirective {
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) {}
+
+  @Input()
+  set auModalOpenOnClick(els: HTMLButtonElement[] | HTMLButtonElement) {
+
+    if (els instanceof HTMLButtonElement) {
+      els = [els]
+    }
+
+    els.forEach(() => {
+      addEventListener('click', () => {
+        this.viewContainer.clear()
+        this.viewContainer.createEmbeddedView(this.templateRef)
+      })
+    })
+  }
+
+
+}
