24855334040620329e66319d93a2d16218e0ab42
Massimiliano Brero <massimiliano.brero@adesso.de>
2022-10-08 18:50:02 +0200

    :host an encapsulation

--- ng-animations/ng-animations-1/src/app/app.module.ts
+++ ng-animations/ng-animations-1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
