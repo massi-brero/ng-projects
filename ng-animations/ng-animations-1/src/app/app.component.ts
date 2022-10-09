810c54fb5ab328d13a9c22b1984506ad7eda81b6
Massimiliano Brero <massimiliano.brero@adesso.de>
2022-10-09 17:58:06 +0200

    more pseudo selectors

--- ng-animations/ng-animations-1/src/app/app.component.ts
+++ ng-animations/ng-animations-1/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-animations-1';
+
+  isFavorite = false;
+  isBoring = false;
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite
+  }
+
+  toggleBoring() {
+    this.isBoring = !this.isBoring;
+  }
 }
--- ng-animations/ng-animations-1/src/app/author/author.component.ts
+++ ng-animations/ng-animations-1/src/app/author/author.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
     h1 {
       margin: 0;
       font-size: 12px;
-      color: #ccc
+      //color: #ccc
     }
   `],
 })
