04842111bef681a0a56e404ad0d1e21a9374dc80
massi.brero@gmail.com <massi.brero@gmail.com>
2022-11-06 19:32:03 +0100

    forms: typed & non nullable

--- forms/courses-original/src/app/login-reactive/login-reactive.component.ts
+++ forms/courses-original/src/app/login-reactive/login-reactive.component.ts
@@ -1,24 +1,50 @@
 import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms'
+import {
+    FormBuilder,
+    FormGroup,
+    NonNullableFormBuilder,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms'
+import { createPasswordStrengthValidator } from '../validators/password-strength.validator'
 
 @Component({
     selector: 'login',
     templateUrl: './login-reactive.component.html',
     styleUrls: ['./login-reactive.component.css'],
 })
-export class LoginReactiveComponent implements OnInit {
-    loginForm: FormGroup
+export class LoginReactiveComponent {
+    loginForm = this.initForm()
 
-    constructor(private fb: FormBuilder) {}
-
-    ngOnInit() {
-        this.initForm()
-    }
+    constructor(private fb: NonNullableFormBuilder) {}
 
     private initForm() {
-        this.loginForm = this.fb.group({
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.min(8)]],
+        return this.fb.group({
+            email: [
+                '',
+                [Validators.required, Validators.email],
+                null,
+                { updateOn: 'blur' },
+            ],
+            password: [
+                '',
+                [
+                    Validators.required,
+                    Validators.min(8),
+                    createPasswordStrengthValidator(),
+                ],
+                null,
+            ],
         })
+    }
+
+    login() {
+        const formValue = this.loginForm.value
+
+        this.loginForm.patchValue({})
+    }
+
+    reset() {
+        this.loginForm.reset()
     }
 }
