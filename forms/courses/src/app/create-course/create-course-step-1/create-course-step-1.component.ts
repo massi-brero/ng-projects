42f4d17b4d033ad9b8fc130d1399736359a4dcc0
massi.brero@gmail.com <massi.brero@gmail.com>
2022-11-20 16:02:41 +0100

    forms: custom input element

--- forms/courses/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ forms/courses/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,52 +1,61 @@
-import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import { courseTitleValidator } from '../../validators/course-title-validator'
-import { CoursesService } from '../../services/courses.service'
-import { Observable } from 'rxjs'
-import { CourseCategory } from '../../types/courseCategory'
+import {Component, OnInit} from '@angular/core'
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import {courseTitleValidator} from '../../validators/course-title-validator'
+import {CoursesService} from '../../services/courses.service'
+import {Observable} from 'rxjs'
+import {filter} from 'rxjs/operators'
+import {CourseCategory} from '../../types/courseCategory'
 
 @Component({
-    selector: 'create-course-step-1',
-    templateUrl: './create-course-step-1.component.html',
-    styleUrls: ['./create-course-step-1.component.scss'],
+  selector: 'create-course-step-1',
+  templateUrl: './create-course-step-1.component.html',
+  styleUrls: ['./create-course-step-1.component.scss'],
 })
 export class CreateCourseStep1Component implements OnInit {
-    form: FormGroup = this.initForm()
-    titleMinLength = 5
-    titleMaxLength = 60
-    courseCategories$: Observable<CourseCategory[]>
+  form: FormGroup = this.initForm()
+  titleMinLength = 5
+  titleMaxLength = 60
+  courseCategories$: Observable<CourseCategory[]>
 
-    constructor(private fb: FormBuilder, private courses: CoursesService) {}
+  constructor(private fb: FormBuilder, private courses: CoursesService) {
+  }
 
-    ngOnInit() {
-        this.courseCategories$ = this.courses.findCourseCategories()
-    }
+  get courseTitle() {
+    return this.form.get('title')
+  }
 
-    get courseTitle() {
-        return this.form.get('title')
-    }
+  ngOnInit() {
+    this.courseCategories$ = this.courses.findCourseCategories()
+    const draft = JSON.parse(localStorage.getItem('STEP_1'))
 
-    private initForm() {
-        return this.fb.group({
-            title: [
-                '',
-                {
-                    validators: [
-                        Validators.required,
-                        Validators.minLength(this.titleMinLength),
-                        Validators.maxLength(this.titleMaxLength),
-                    ],
-                    asyncValidators: [courseTitleValidator(this.courses)],
-                    updateOn: 'blur',
-                },
-            ],
-            releasedAt: [new Date(), Validators.required],
-            downloadsAllowed: [false, Validators.requiredTrue],
-            longDescription: [
-                '',
-                [Validators.required, Validators.minLength(3)],
-            ],
-            category: ['BEGINNER', Validators.required],
-        })
-    }
+    draft ? this.form.setValue(draft) : null;
+
+    this.form.valueChanges
+      .pipe(filter((_) => this.form.valid))
+      .subscribe(_ => localStorage.setItem('STEP_1', JSON.stringify(this.form.value)))
+  }
+
+  private initForm() {
+    return this.fb.group({
+      title: [
+        '',
+        {
+          validators: [
+            Validators.required,
+            Validators.minLength(this.titleMinLength),
+            Validators.maxLength(this.titleMaxLength),
+          ],
+          asyncValidators: [courseTitleValidator(this.courses)],
+          updateOn: 'blur',
+        },
+      ],
+      releasedAt: [new Date(), Validators.required],
+      downloadsAllowed: [false, Validators.requiredTrue],
+      longDescription: [
+        '',
+        [Validators.required, Validators.minLength(3)],
+      ],
+      category: ['BEGINNER', Validators.required],
+    })
+  }
 }
--- forms/courses/src/app/file-upload/file-upload.component.ts
+++ forms/courses/src/app/file-upload/file-upload.component.ts
@@ -1,16 +1,17 @@
-import {Component, Input} from '@angular/core';
-import {HttpClient, HttpEventType} from '@angular/common/http';
-import {catchError, finalize} from 'rxjs/operators';
-import {AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator} from '@angular/forms';
-import {noop, of} from 'rxjs';
-
+import { Component, Input } from '@angular/core'
 
 @Component({
-  selector: 'file-upload',
-  templateUrl: "file-upload.component.html",
-  styleUrls: ["file-upload.component.scss"]
+    selector: 'file-upload',
+    templateUrl: 'file-upload.component.html',
+    styleUrls: ['file-upload.component.scss'],
 })
 export class FileUploadComponent {
+    @Input() requiredFileType: string
+    filename = ''
 
-
+  // @ts-ignore
+  onFileSelected(event: Event) {
+    console.log(event);
+    const file: File = (event.target as HTMLInputElement).files[0]
+  }
 }
