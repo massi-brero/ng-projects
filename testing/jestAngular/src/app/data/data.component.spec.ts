bac7f94b2493700f4159fc8c48b7294b5fdb5a24
massi.brero@gmail.com <massi.brero@gmail.com>
2022-12-20 23:14:24 +0100

    jest-tutorial: service testing 2

--- testing/jestAngular/src/app/data/data.component.spec.ts
+++ testing/jestAngular/src/app/data/data.component.spec.ts
@@ -1,23 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { DataComponent } from './data.component';
+import { DataComponent } from './data.component'
+import { FakeService } from '../services/fake.service'
+import { of } from 'rxjs'
 
 describe('DataComponent', () => {
-  let component: DataComponent;
-  let fixture: ComponentFixture<DataComponent>;
+  let component: DataComponent
+  let fixture: ComponentFixture<DataComponent>
+  let fakeServiceMock: any
 
   beforeEach(async () => {
+    fakeServiceMock = {
+      getDataV1: jest.fn(),
+    }
     await TestBed.configureTestingModule({
-      declarations: [ DataComponent ]
-    })
-    .compileComponents();
+      declarations: [DataComponent],
+      providers: [
+        {
+          provide: FakeService,
+          useValue: fakeServiceMock,
+        },
+      ],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(DataComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(DataComponent)
+    component = fixture.componentInstance
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should return service data', () => {
+    const expectedResponse = { name: 'Massi' }
+    jest
+      .spyOn(fakeServiceMock, 'getDataV1')
+      .mockReturnValue(of(expectedResponse))
+    fixture.detectChanges()
+
+    expect(component.serviceData.name).toBe(expectedResponse.name)
+  })
+})
--- testing/jestAngular/src/app/data/data.component.ts
+++ testing/jestAngular/src/app/data/data.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FakeService } from '../services/fake.service'
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styleUrls: ['./data.component.scss']
+  styleUrls: ['./data.component.scss'],
 })
 export class DataComponent implements OnInit {
+  serviceData: any
+  errorMessage: any
 
-  constructor() { }
+  constructor(private fakeService: FakeService) {}
 
   ngOnInit(): void {
+    this.getServicedata()
   }
 
+  private getServicedata() {
+    this.fakeService.getDataV1().subscribe(
+      (data) => {
+        this.serviceData = data
+      },
+      (error) => {
+        this.errorMessage = error.statusText
+      },
+      () => {
+        console.log('finished')
+      }
+    )
+  }
 }
