f984b92599a960de398286ff65ce75ee1c94cb7e
massi.brero@gmail.com <massi.brero@gmail.com>
2023-01-03 22:44:46 +0100

    stateless response pattern & shareReplay

---
+++ advanced/rxjs/reactive-angular-course/src/app/services/courses.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpResponse } from '@angular/common/http'
+import { Course } from '../model/course'
+import { Observable } from 'rxjs'
+import { environment } from '../../environments/environment'
+import { map, shareReplay } from 'rxjs/operators'
+
+@Injectable({ providedIn: 'root' })
+export class CoursesService {
+  constructor(private http: HttpClient) {}
+  loadAllCourses(): Observable<Course[]> {
+    return this.http
+      .get<HttpResponse<Course[]>>(environment.apiCoursesUrl)
+      .pipe(
+        map((res: HttpResponse<Course[]>) => {
+          return res['payload']
+        }),
+        shareReplay()
+      )
+  }
+}
