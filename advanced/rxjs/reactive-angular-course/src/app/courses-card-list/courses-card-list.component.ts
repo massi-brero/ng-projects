41118de52c79d60352fd507513019c7bd83f4525
massi.brero@gmail.com <massi.brero@gmail.com>
2023-01-04 23:26:22 +0100

    rxJs: presentational pattern & data mods

---
+++ advanced/rxjs/reactive-angular-course/src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input } from '@angular/core'
+import { Course } from '../model/course'
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog'
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component'
+import { filter, tap } from 'rxjs/operators'
+
+@Component({
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.scss'],
+})
+export class CoursesCardListComponent {
+  @Input() courses: Course[] = []
+
+  private coursesChanged = new EventEmitter()
+
+  constructor(private dialog: MatDialog) {}
+
+  editCourse(course: Course) {
+    const dialogConfig = new MatDialogConfig()
+
+    dialogConfig.disableClose = true
+    dialogConfig.autoFocus = true
+    dialogConfig.width = '400px'
+
+    dialogConfig.data = course
+
+    this.dialog
+      .open(CourseDialogComponent, dialogConfig)
+      .afterClosed()
+      .pipe(
+        filter((val) => !!val),
+        tap(() => this.coursesChanged.emit())
+      )
+      .subscribe()
+  }
+}
