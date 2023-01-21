bc7375dec778f76505646e385870e0d84f5c4f13
massi.brero@gmail.com <massi.brero@gmail.com>
2023-01-21 23:02:08 +0100

    store 1

--- advanced/rxjs/reactive-angular-course/src/app/home/home.component.ts
+++ advanced/rxjs/reactive-angular-course/src/app/home/home.component.ts
@@ -1,22 +1,13 @@
-import { Component, OnInit } from '@angular/core'
-import { Course, sortCoursesBySeqNo } from '../model/course'
-import { interval, noop, Observable, of, throwError, timer } from 'rxjs'
-import {
-  catchError,
-  delay,
-  delayWhen,
-  filter,
-  finalize,
-  map,
-  retryWhen,
-  shareReplay,
-  tap,
-} from 'rxjs/operators'
-import { HttpClient } from '@angular/common/http'
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog'
-import { CourseDialogComponent } from '../course-dialog/course-dialog.component'
-import { CoursesService } from '../services/courses.service'
-import { LoadingService } from '../services/loading.service'
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { MessagesService } from '../messages/services/messages.service';
+import { Course, sortCoursesBySeqNo } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { CoursesStore } from '../services/courses.store';
+import { LoadingService } from '../services/loading.service';
 
 @Component({
   selector: 'home',
@@ -25,35 +16,16 @@ import { LoadingService } from '../services/loading.service'
 })
 export class HomeComponent implements OnInit {
   beginnerCourses$: Observable<Course[]>
-
   advancedCourses$: Observable<Course[]>
 
-  constructor(
-    private http: HttpClient,
-    private coursesService: CoursesService,
-    private loadingService: LoadingService
-  ) {}
+  constructor(private coursesStore: CoursesStore) {}
 
   ngOnInit() {
     this.loadCourses()
   }
 
   loadCourses() {
-    this.loadingService.loadingOn()
-    const courses$ = this.coursesService.loadAllCourses().pipe(
-      map((courses: Course[]) => courses.sort(sortCoursesBySeqNo)),
-      finalize(() => this.loadingService.loadingOff())
-    )
-
-    this.beginnerCourses$ = courses$.pipe(
-      map((courses: Course[]) =>
-        courses.filter((course) => course.category === 'BEGINNER')
-      )
-    )
-    this.advancedCourses$ = courses$.pipe(
-      map((courses: Course[]) =>
-        courses.filter((course) => course.category === 'ADVANCED')
-      )
-    )
+    this.beginnerCourses$ = this.coursesStore.filterByCategory('BEGINNER')
+    this.advancedCourses$ = this.coursesStore.filterByCategory('ADVANCED')
   }
 }
