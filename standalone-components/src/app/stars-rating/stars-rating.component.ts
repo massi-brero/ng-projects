c2bb08cb777ddaa9497aafbe0a56391082d86170
massi.brero@gmail.com <massi.brero@gmail.com>
2023-09-30 16:12:29 +0200

    first commit

---
+++ standalone-components/src/app/stars-rating/stars-rating.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stars-rating',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stars-rating.component.html',
+  styleUrls: ['./stars-rating.component.scss']
+})
+export class StarsRatingComponent {
+
+}
