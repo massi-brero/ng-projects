f1eb605d9cf55b3a8cf1341a013cc543856efa25
massi.brero@gmail.com <massi.brero@gmail.com>
2023-10-01 15:30:06 +0200

    erte version abgeschlossen

--- standalone-components/src/app/app.component.ts
+++ standalone-components/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { StarsRatingComponent } from './stars-rating/stars-rating.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    NgSwitch,
+    NgSwitchDefault,
+    NgSwitchCase,
+    RouterOutlet,
+    StarsRatingComponent,
+  ],
 })
 export class AppComponent {
   title = 'standalone-components';
--- standalone-components/src/app/stars-rating/stars-rating.component.ts
+++ standalone-components/src/app/stars-rating/stars-rating.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+enum STAR_STATE {
+  EMPTY,
+  HALF,
+  FULL,
+}
+
+interface Star {
+  state: STAR_STATE;
+}
 
 @Component({
   selector: 'app-stars-rating',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './stars-rating.component.html',
-  styleUrls: ['./stars-rating.component.scss']
+  styleUrls: ['./stars-rating.component.scss'],
 })
-export class StarsRatingComponent {
+export class StarsRatingComponent implements OnInit {
+  @Input() rating = 0;
+  starsRow: Star[] = [
+    { state: STAR_STATE.EMPTY },
+    { state: STAR_STATE.EMPTY },
+    { state: STAR_STATE.EMPTY },
+    { state: STAR_STATE.EMPTY },
+    { state: STAR_STATE.EMPTY },
+  ];
 
+  ngOnInit(): void {
+    console.log(this.rating.toString().split('.'));
+
+    if (
+      this.rating > 5 ||
+      this.rating < 0 ||
+      this.rating.toString().split('.')[1] !== '5'
+    ) {
+      console.error('Only half steps and numbers from 0-5 are allowed');
+      return;
+    }
+
+    let idx = 0;
+    while (this.rating > 0) {
+      if (this.rating === 0.5) {
+        this.starsRow[idx].state = STAR_STATE.HALF;
+        break;
+      }
+      this.starsRow[idx].state = STAR_STATE.FULL;
+      this.rating -= 1;
+      idx += 1;
+    }
+
+    console.log(this.starsRow);
+  }
 }
