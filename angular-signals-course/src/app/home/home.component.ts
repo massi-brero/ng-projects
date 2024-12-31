0680cb654af1f703b5283b19f150fdd6034bf4dd
massi.brero@gmail.com <massi.brero@gmail.com>
2024-12-31 23:05:41 +0100

    deletes .git dolder

---
+++ angular-signals-course/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
+import {CoursesService} from "../services/courses.service";
+import {Course, sortCoursesBySeqNo} from "../models/course.model";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
+import {MatDialog} from "@angular/material/dialog";
+import {MessagesService} from "../messages/messages.service";
+import {catchError, from, throwError} from "rxjs";
+import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+
+@Component({
+    selector: 'home',
+    imports: [
+        MatTabGroup,
+        MatTab,
+        CoursesCardListComponent
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  values= signal<number[]>([0])
+
+  append() {
+    this.values.update(values => ([
+      ...values,
+      values[values.length - 1] + 1
+    ]))
+  }
+}
