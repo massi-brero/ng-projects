34d32beaedc0be28d1edf05dd70ca7dbdc4a0594
massi.brero@gmail.com <massi.brero@gmail.com>
2025-01-04 21:33:47 +0100

    playing with effects

--- angular-signals-course/src/app/home/home.component.ts
+++ angular-signals-course/src/app/home/home.component.ts
@@ -1,30 +1,29 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { Component, computed, effect, inject, Injector, signal } from '@angular/core'
+import { CoursesService } from '../services/courses.service'
+import { Course, sortCoursesBySeqNo } from '../models/course.model'
+import { MatTab, MatTabGroup } from '@angular/material/tabs'
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component'
+import { MatDialog } from '@angular/material/dialog'
+import { MessagesService } from '../messages/messages.service'
+import { catchError, from, throwError } from 'rxjs'
+import { toObservable, toSignal, outputToObservable, outputFromObservable } from '@angular/core/rxjs-interop'
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  values= signal<number[]>([0])
+  counter = signal(0)
 
-  append() {
-    this.values.update(values => ([
-      ...values,
-      values[values.length - 1] + 1
-    ]))
+  constructor() {
+    effect(() => {
+      console.log('counter: ', this.counter())
+    })
+  }
+
+  increment() {
+    this.counter.update((val) => val + 1)
   }
 }
