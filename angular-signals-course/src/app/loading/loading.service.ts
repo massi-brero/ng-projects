68fe20f010ecf1db5a98637ffb9d44d516ff410f
massi.brero@gmail.com <massi.brero@gmail.com>
2025-01-19 17:15:42 +0100

    start loader

--- angular-signals-course/src/app/loading/loading.service.ts
+++ angular-signals-course/src/app/loading/loading.service.ts
@@ -1,8 +1,8 @@
-import {Injectable, signal} from "@angular/core";
+import { Injectable, signal } from '@angular/core'
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class LoadingService {
-
+  loadingOn() {}
 }
