969250efe12a192e7a9ba7694f849c022bf45354
massi.brero@gmail.com <massi.brero@gmail.com>
2026-01-27 20:35:51 +0100

    start signal forms

--- signals/angular-signals/practice/18. fun-with-forms - basic/src/app/app.ts
+++ signals/angular-signals/practice/18. fun-with-forms - basic/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DinnerReview } from './models/dinner-review.model';
+import { email, form, FormField, minLength, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormField],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -15,5 +16,11 @@ export class App {
     email: '',
     description: '',
     rating: 1,
-  })
+  });
+  readonly reviewForm = form(this.model, (path) => {
+    required(path.username);
+    required(path.email);
+    email(path.email);
+    minLength(path.description, 10);
+  });
 }
