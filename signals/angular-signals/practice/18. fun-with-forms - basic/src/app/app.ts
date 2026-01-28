43e63c3e36fc750ae3b59e93fd2de3f25d397a0c
massi.brero@gmail.com <massi.brero@gmail.com>
2026-01-28 11:03:56 +0100

    Validation of signal forms

--- signals/angular-signals/practice/18. fun-with-forms - basic/src/app/app.ts
+++ signals/angular-signals/practice/18. fun-with-forms - basic/src/app/app.ts
@@ -1,7 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DinnerReview } from './models/dinner-review.model';
-import { email, form, FormField, minLength, required } from '@angular/forms/signals';
+import {
+  email,
+  FieldContext,
+  FieldValidator,
+  form,
+  FormField,
+  minLength,
+  required,
+  validate,
+} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +26,23 @@ export class App {
     description: '',
     rating: 1,
   });
+
   readonly reviewForm = form(this.model, (path) => {
-    required(path.username);
-    required(path.email);
-    email(path.email);
-    minLength(path.description, 10);
+    required(path.username, { message: 'Username is required' });
+    required(path.email, { message: 'Email is required' });
+    email(path.email, { message: 'Email must be valid' });
+
+    validate(path.description, ({ value }) => {
+      const wordArray = value().trim().split(/\s+/);
+      const wordCount = (wordArray.length = 1 && wordArray[0] === '' ? 0 : wordArray.length);
+      console.log(value().trim().split(/\s+/));
+
+      return wordCount >= 10
+        ? null
+        : {
+            kind: 'minWords',
+            message: `Description must be at least 10 words long (Currently there are ${wordCount} words)`,
+          };
+    });
   });
 }
