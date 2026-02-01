4b0c72b3013ab7486e697b3827bd652a5be1a30b
massi.brero@gmail.com <massi.brero@gmail.com>
2026-02-01 15:34:46 +0100

    Fprm Araay validation

--- signals/angular-signals/practice/21. fun-with-forms - arrays/src/app/app.ts
+++ signals/angular-signals/practice/21. fun-with-forms - arrays/src/app/app.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import {
+  applyEach,
   email,
   FormField as Field,
   form,
   required,
   validate,
   ValidationError,
+  min,
+  max,
+  validateTree,
 } from '@angular/forms/signals';
 import { DinnerReview } from './models/dinner-review.model';
 
@@ -57,6 +61,18 @@ export class App {
     }));
   }
 
+  /**
+   * Reactive form configuration for user reviews with validation rules.
+   *
+   * Validates the following fields:
+   * - `username`: Required field
+   * - `email`: Required when role is not 'author', must be in valid email format
+   * - `description`: Custom validation requiring minimum word count (10 words for authors, 5 for others)
+   * - `reviews[].rating`: Array of ratings with minimum value of 1 and maximum value of 5
+   *
+   * @readonly
+   * @type {Form}
+   */
   readonly reviewForm = form(this.model, (path) => {
     required(path.username, {
       message: 'Username is required',
@@ -83,6 +99,38 @@ export class App {
       }
 
       return undefined;
+    });
+
+    applyEach(path.reviews, (reviewPath) => {
+      min(reviewPath.rating, 1, {
+        message: 'Minimum is 1.',
+      });
+      max(reviewPath.rating, 5, {
+        message: 'Maximum is 5.',
+      });
+      required(reviewPath.aspect, {
+        message: 'Aspect is required.',
+      });
+      validateTree(reviewPath, (ctx) => {
+        const rating = ctx.valueOf(reviewPath.rating);
+        const recommendation = ctx.valueOf(reviewPath.recommendation);
+
+        if (rating >= 4 && recommendation === 'not-recommend') {
+          return [
+            {
+              kind: 'inconsistent-rating-recommendation',
+              message: 'High ratings should have a positive recommendation',
+              fieldTree: ctx.fieldTreeOf(reviewPath.rating),
+            } as ValidationError.WithOptionalField,
+            {
+              kind: 'inconsistent-rating-recommendation',
+              message: 'High ratings should have a positive recommendation',
+              fieldTree: ctx.fieldTreeOf(reviewPath.recommendation),
+            } as ValidationError.WithOptionalField,
+          ];
+        }
+        return undefined;
+      });
     });
   });
 }
